f81e3cfb42241a248e701180722ee64a0a183371
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-15 18:32:55 -0300

    feat: adding props to useState

--- src/modulo3/hooks/useState.js
+++ src/modulo3/hooks/useState.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 const App = () => {
     const  [ativo, setAtivo] = React.useState(false)
+    const[dados, setDados] = React.useState({nome: 'Maria', idade: '20'})
 
     function handleClick() {
         setAtivo(!ativo)
+        setDados({...dados, faculdade: 'Possui faculdade'})
     }
-  return <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'} </button>
+  return (
+      <div>
+          <p>{dados.nome}</p>
+          <p>{dados.idade}</p>
+          <p>{dados.faculdade}</p>
+          <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'} </button>
+      </div>
+  )
 }
 
 export default App
