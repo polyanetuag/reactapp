b6b8a27a31275d66547053b7cf4366828ae0b21e
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-16 15:11:22 -0300

    feat: using fetch

---
+++ src/modulo3/Exercício1/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Product from './Product'
+
+function App() {
+    const [dados, setDados] = React.useState(null)
+    const [carregando, setCarregando] = React.useState(null)
+
+    async function handleClick(event) {
+        setCarregando(true)
+        const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+        )
+        const json = await response.json()
+        setDados(json)
+        setCarregando(false)
+    }
+
+  return (
+    <div>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>Notebook</button>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>Smartphone</button>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>Tablet</button>
+        {carregando && <p>Carregando...</p>}
+        {!carregando && dados && <Product dados={dados} />}
+    </div>
+  )
+}
+
+export default App
