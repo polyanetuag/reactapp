ade299268a88e6d7a52cb765c6744407c79bf0e4
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-18 20:22:21 -0300

    feat: useRef real example

---
+++ src/modulo3/hooks/useRef2.js
@@ -0,0 +1,28 @@
+import React from 'react'
+//exemplo real -> carrinho de compras
+
+const App = () => {
+    const [carrinho, setCarrinho] = React.useState(0)
+    const [notificacao, setNotificacao] = React.useState(null)
+    const timeoutRef = React.useRef()
+
+    function handleClick() {
+        setCarrinho(carrinho + 1)
+        setNotificacao('Produto adicionado ao carrinho')
+
+        clearTimeout(timeoutRef.current)
+        timeoutRef.current = setTimeout(() => {
+            setNotificacao(null)
+        }
+        , 2000)
+    }
+
+  return (
+    <div>
+        <p>{notificacao}</p>
+        <button onClick={handleClick}>Adicionar Carrinho {carrinho}</button>
+    </div>
+  )
+}
+
+export default App
