0b9bb05bbe37ae847ec27de6b404ce83d09a455d
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-21 12:19:35 -0300

    feat: creating a useFetch hook

--- src/modulo3/customHooks/App.js
+++ src/modulo3/customHooks/App.js
@@ -1,18 +1,42 @@
-import React from 'react'
-import useLocalStorage from './useLocalStorage'
+import React from "react";
+import useFetch from "./useFetch";
+import useLocalStorage from "./useLocalStorage";
 
 const App = () => {
-    const [produto, setProduto] = useLocalStorage('produto', '')
+  const [produto, setProduto] = useLocalStorage("produto", "");
 
-    function handleClick({ target }) {
-        setProduto(target.innerText)
+  const { request, data, loading, error } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const { response, json } = await request(
+        "https://ranekapi.origamid.dev/json/api/produto"
+      );
     }
-  return (
-    <div>
+
+    fetchData();
+  }, [request]);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data)
+    return (
+      <div>
+        <p>Produto perferido:{produto} </p>
         <button onClick={handleClick}>Notebook</button>
         <button onClick={handleClick}>Smartphone</button>
-    </div>
-  )
-}
 
-export default App
+        {data.map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  else return null;
+};
+
+export default App;
---
+++ src/modulo3/customHooks/useFetch.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json;
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      setLoading(false);
+    } catch (error) {
+      json = null;
+      setError("Erro ao carregar dados");
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return { data, error, loading, request };
+};
+
+export default useFetch;
