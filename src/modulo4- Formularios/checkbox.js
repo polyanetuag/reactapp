8dc88da42e50a627d440e58150bd58b7f1e03658
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-23 14:38:01 -0300

    fix: checking checkbox reactive state

--- src/modulo4- Formularios/checkbox.js
+++ src/modulo4- Formularios/checkbox.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 const App = () => {
-  const [termos, setTermos] = React.useState(false);
+  const [cores, setCores] = React.useState(['azul', 'vermelho']);
+
+  function handleChange({target}) {
+    if (target.checked) {
+      setCores([...cores, target.value]); //verifica se o valor já existe no array, se não existir, adiciona
+    } else {
+      setCores(cores.filter(cor => cor !== target.value)); //removendo o valor do array
+    }
+  }
 
   return (
     <form>
       <label>
         <input
           type="checkbox"
-          value="Termos"
-          checked={termos}
-          onChange={({ target }) => setTermos(target.checked)}
+          value="azul"
+          checked={cores.includes('azul')}
+          onChange={handleChange}
         />
-        Aceito os termos
+        Azul
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="vermelho"
+          checked={cores.includes('vermelho')}
+          onChange={handleChange}
+        />
+        Vermelho
       </label>
     </form>
   );
