aa1964b02a1700f62a7a92b41b7f886ddafcab44
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-24 11:26:15 -0300

    feat: rendering select component

--- src/modulo4- Formularios/Componentes/App.js
+++ src/modulo4- Formularios/Componentes/App.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import Input from './Input'
+import React from "react";
+import Input from "./Form/Input";
+import Select from "./Form/Select";
 
 const App = () => {
-    const [nome, setNome] = React.useState('')
-    const [email, setEmail] = React.useState('')
+  const [nome, setNome] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [produto, setProduto] = React.useState("");
 
   return (
     <form>
-        <Input id='nome' label='Nome' value={nome} setValue={setNome} required />
-        <Input id='email' label='Email' value={email} setValue={setEmail}/>
+      <Select
+        options={["smartphone", "tablet"]}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
+      <Input id="email" label="Email" value={email} setValue={setEmail} />
     </form>
-  )
-}
+  );
+};
 
-export default App
+export default App;
