30e6d8dc19d6c114427d730db00966d32236a10f
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-24 13:30:35 -0300

    feat: rendering radio component

--- src/modulo4- Formularios/Componentes/App.js
+++ src/modulo4- Formularios/Componentes/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Input from "./Form/Input";
+import Radio from "./Form/Radio";
 import Select from "./Form/Select";
 
 const App = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [produto, setProduto] = React.useState("");
+  const [cor, setCor] = React.useState("");
 
   return (
     <form>
+        <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
       <Select
         options={["smartphone", "tablet"]}
         value={produto}
