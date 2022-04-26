d2198deac08fd67d272de1ab86376e563b286f4b
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-26 19:20:01 -0300

    feat: rendered useForm

--- src/modulo4- Formularios/Componentes/App.js
+++ src/modulo4- Formularios/Componentes/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Checkbox from "./Form/Checkbox";
+import useForm from "./Form/Hooks/useForm";
 import Input from "./Form/Input";
 import Radio from "./Form/Radio";
 import Select from "./Form/Select";
@@ -10,25 +11,37 @@ const App = () => {
   const [produto, setProduto] = React.useState("");
   const [cor, setCor] = React.useState("");
   const [linguagens, setLinguagens] = React.useState([]);
-  const [cep, setCep] = React.useState("");
+  // const [cep, setCep] = React.useState("");
   const [error, setError] = React.useState(null);
 
-  function validateCep(value) {
-    if(value.length === 0) {
-      setError('Preencha o CEP');
-      return false;
-    } else if(!/^\d{5}-\d{3}$/.test(value)) {
-      setError('Preencha um CEP válido');
-      return false;
-    } else {
-      setError(null);
-      return true;
+  const cep = useForm('cep');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (true
+    ) {
+      setError("Preencha todos os campos");
+      return;
     }
+    setError(null);
   }
 
-  function handleBlur({target}) {
-    validateCep(target.value);
-  }
+  // function validateCep(value) {
+  //   if(value.length === 0) {
+  //     setError('Preencha o CEP');
+  //     return false;
+  //   } else if(!/^\d{5}-\d{3}$/.test(value)) {
+  //     setError('Preencha um CEP válido');
+  //     return false;
+  //   } else {
+  //     setError(null);
+  //     return true;
+  //   }
+  // }
+
+  // function handleBlur({target}) {
+  //   validateCep(target.value);
+  // }
 
   return (
     <form>
@@ -52,8 +65,8 @@ const App = () => {
         id='cep'
         type='text'
         value={cep}
-        setValue={setCep}
-        onBlur={handleBlur}
+        // setValue={setCep}
+        // onBlur={handleBlur}
         placeholder='000000-000'
         />
         {error && <span style={{color: 'red'}}>{error}</span>}
