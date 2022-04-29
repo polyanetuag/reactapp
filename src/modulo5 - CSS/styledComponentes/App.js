8a6713b9e08fa056398cbeb1adb43de2f566b30d
Polyane Tuag <pollyportugues@hotmail.com>
2022-04-29 18:29:28 -0300

    feat: using styled components

---
+++ src/modulo5 - CSS/styledComponentes/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  font-weight: bold;
+`;
+
+const App = () => {
+  return <div>
+      <Title>My Title</Title>
+  </div>;
+};
+
+export default App;
