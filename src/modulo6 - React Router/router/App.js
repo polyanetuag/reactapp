528493615a9ee0d6b5ccd93e9bcc7c75ed78d45b
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-01 12:04:47 -0300

    feat: route Login

--- src/modulo6 - React Router/router/App.js
+++ src/modulo6 - React Router/router/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Header from "./Header";
+import Login from "./Login";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/modulo6 - React Router/router/Header.js
+++ src/modulo6 - React Router/router/Header.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+// import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom"; // possui o mesmo comportamento do Link do react-router-dom só que mostra o elemento ativo
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      <NavLink to="/" activeStyle={{color: 'tomato'}} end>
+        Home
+      </NavLink>{" "}
+      | <NavLink to="/about" activeStyle={{color: 'tomato'}}>About</NavLink>{" "}
+      | <NavLink to="/login" activeStyle={{color: 'tomato'}}>Login</NavLink>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
